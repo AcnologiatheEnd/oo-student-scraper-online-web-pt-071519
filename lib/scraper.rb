dca0d1b1d31d6eacf93b91950687e03973fb1431
Winston  Chung <18.chung.winston@ctkprep.org>
2019-09-29 20:53:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,27 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    students = []
+    html = open(index_url)
+    index = Nokogiri::HTML(html)
+    index.css("div.student-card").each do |student|
+      student_details = {}
+      student_details[:name] = student.css("h4.student-name").text
+      student_details[:location] = student.css("p.student-location").text
+      profile_path = student.css("a").attribute("href").value
+      student_details[:profile_url] = './fixtures/student-site/' + profile_path
+      students << student_details
+    end
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    student_profile = {}
+    html = open(profile_url)
+    profile = Nokogiri::HTML(html)
   end
 
 end
